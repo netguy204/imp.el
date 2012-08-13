f8ebed575805bbe197a21e3fd5776f32248aa7b6
Brian Taylor <el.wubo@gmail.com>
2012-08-12 22:19:50 -0400

    cleanup

---
+++ README.md
@@ -0,0 +1,14 @@
+Impatient Mode
+==============
+
+See the effect of your HTML as you type it.
+
+This mode uses the simple-httpd webserver that runs within emacs to
+serve up your HTML buffers as you edit them.
+
+Set the buffer you want to live edit with:
+
+M-x imp-set-current-buffer
+
+And then point your browser to http://localhost:8080/imp-shim and
+watch your changes appear as you type!
--- imp.el
+++ imp.el
@@ -1,13 +1,15 @@
 (require 'simple-httpd)
 
-;; serve up the shim that will do the long polling
 (defvar imp-shim-root "~/.emacs.d/imp/")
 (defvar imp-current-buffer nil)
 
 (defun httpd/imp-shim (proc path &rest args)
   (let* ((file (file-name-nondirectory path))
-         (clean (expand-file-name file imp-shim-root)))
-    (httpd-send-file proc clean)))
+         (clean (expand-file-name file imp-shim-root))
+         (index (expand-file-name "index.html" imp-shim-root)))
+    (cond
+     ((file-directory-p clean) (httpd-send-file proc index))
+     (t (httpd-send-file proc clean)))))
 
 (defservlet imp text/html (path)
   (if imp-current-buffer
@@ -15,6 +17,7 @@
     (insert "run imp-set-current-buffer with the buffer you want to monitor")))
 
 (defun imp-set-current-buffer (buffer)
+  "sets BUFFER to be the buffer watched for changes by imp"
   (interactive "bbuffer:")
   (setq imp-current-buffer buffer))
 
